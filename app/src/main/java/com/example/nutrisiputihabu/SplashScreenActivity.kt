128f867987fa33bd273e99f62639ef6ec2d14e09
ali <aliansyahptra@gmail.com>
2024-10-28 11:11:59 +0700

    splashscreen edit

---
+++ app/src/main/java/com/example/nutrisiputihabu/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.example.nutrisiputihabu
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         AppTheme {
+
+         }
+    }
+}
