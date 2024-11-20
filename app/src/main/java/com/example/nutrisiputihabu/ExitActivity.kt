19afa21e242b5ef0676814eaa084b353d115f57f
Marsani <marsanix@gmail.com>
2024-11-20 11:00:54 +0700

    update exitactivity, add custom font

--- app/src/main/java/com/example/nutrisiputihabu/ExitActivity.kt
+++ app/src/main/java/com/example/nutrisiputihabu/ExitActivity.kt
@@ -18,12 +18,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.res.painterResource
-
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 
 
 class ExitActivity : ComponentActivity() {
@@ -48,51 +52,103 @@ fun GreetingPreview2() {
 
 @Composable
 fun MainScreen() {
+
+    val myFontFamily = FontFamily(
+        Font(R.font.arial_rounded_mt_bold, FontWeight.Normal),
+        Font(R.font.arial_rounded_mt_bold, FontWeight.Bold)
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Bottom,
+            ,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(R.drawable.dialogbox),
+            contentDescription = "",
+            contentScale = ContentScale.FillWidth
+        )
 
-        
-        Button(
-            onClick = { /* Aksi untuk tombol Latihan */ },
-            modifier = Modifier .fillMaxWidth().padding(top = 16.dp, start = 40.dp, end = 40.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Yellow,
-                contentColor = Color.Black
-            )
+        Box(
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text(text = "Yakin nih?",
-                style = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = androidx.compose.ui.text.font.FontFamily.Monospace
+        Image(
+
+            modifier = Modifier
+                .fillMaxHeight()
+                .scale(3f)
+                .align(Alignment.BottomCenter),
+            painter = painterResource(R.drawable.exitbackground),
+            contentDescription = "",
+            contentScale = ContentScale.FillWidth,
+            alignment = Alignment.Center
+        )
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+
+            Button(
+                onClick = { /* Aksi untuk tombol Latihan */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 40.dp, end = 40.dp)
+                    ,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Yellow,
+                    contentColor = Color.Black
                 )
+            ) {
+                Text(
+                    text = "Yakin nih?",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                    ),
+                    fontFamily = myFontFamily,
                 )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Button(
+                onClick = { /* Aksi untuk tombol Tanding */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 40.dp, end = 40.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Yellow,
+                    contentColor = Color.Black
+                )
+            ) {
+                Text(
+                    text = "Cek lagi deh",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = androidx.compose.ui.text.font.FontFamily.Monospace
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(100.dp))
         }
+}
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Button(
-            onClick = { /* Aksi untuk tombol Tanding */ },
-            modifier = Modifier .fillMaxWidth().padding(top = 16.dp, start = 40.dp, end = 40.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Yellow,
-                contentColor = Color.Black
-            )
-        ) {
-            Text(text = "Cek lagi deh",
-                style = TextStyle(
-                    fontWeight = FontWeight.Bold ,
-                    fontFamily = androidx.compose.ui.text.font.FontFamily.Monospace
-                )
-                )
-                   }
     }
 }
 
